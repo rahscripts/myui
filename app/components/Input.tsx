2958ae8874c46a465fa017381a9f5f473cf85c5f
rahscripts <rahmantjis@gmail.com>
2026-01-16 22:54:23 +0530

    product card micro

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -3,8 +3,9 @@
 import { useState } from "react";
 
 type M = [
-    sub: string,
-    amount: string
+   { sub: string,
+    amount: string,
+    color: number,}
 ]
 
 const Input = () => {
@@ -12,12 +13,26 @@ const Input = () => {
     const [sub, setSub] = useState("");
     const [amount, setAmount] = useState("");
 
-    const [mobject, setMobject]= useState<M | []>([]);
+    const [mobject, setMobject]= useState<M>([]);
 
+    const colorOn = [
+        "bg-green-200",
+       "bg-red-200",
+         "bg-blue-200",
+       "bg-purple-200",
+         "bg-indigo-200",
+        
+    ]
 
     const handleAdd = () => {
+
+        if (amount === "") return ;
+        if (sub === "") return ;
         const newOn = {
-            sub, amount
+            sub, 
+            amount, 
+            color: Math.floor(Math.random() * 4) + 1, 
+            id: crypto.randomUUID(),
         }
 
         setMobject(prev => [...prev, newOn])
@@ -28,12 +43,22 @@ const Input = () => {
     console.log(mobject);
 
   return (
-    <div className="flex flex-col gap-3s">
-       <input value={sub} onChange={(e) => setSub(e.target.value)} type="text" placeholder="subscription name" className="input" />
-       <input value={amount} onChange={(e) => setAmount(e.target.value)} type="number" placeholder="amount" className="input input-primary" />
-
-       <button className="btn" onClick={handleAdd}>add</button>
-    </div>
+    <section>
+        <div className="flex flex-col gap-3">
+           <input value={sub} onChange={(e) => setSub(e.target.value)} type="text" placeholder="subscription name" className="input" />
+           <input value={amount} onChange={(e) => setAmount(e.target.value)} type="number" placeholder="amount" className="input input-primary" />
+           <button className="btn w-fit" onClick={handleAdd}>add</button>
+        </div>
+        <div className="flex gap-2">
+            {mobject.map((m) => (
+                <div key={m.id} className={`${colorOn[m.color]} flex flex-col items-center justify-center w-fit p-5 rounded-xl `}>
+                    <div className="font-bold uppercase text-3xl">{m.sub}</div>
+                    <div className="opacity-90">{m.amount}rs/month</div>
+                    <div>{12 * Number(m.amount)}rs/year</div>
+                </div>
+            ))}
+        </div>
+    </section>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Input from "./components/Input";
 
 export default function Home() {
   return (
-   <main className="bg-white min-h-screen">
+   <main className="bg-white p-5 min-h-screen">
       <h1>Money tracker</h1>
       <Input />
      </main>
