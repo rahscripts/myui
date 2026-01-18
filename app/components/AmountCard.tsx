c5b5a5625f8d832aa26bbe990f169e22e1d30c4c
rahscripts <rahmantjis@gmail.com>
2026-01-18 18:29:20 +0530

    delete icon

--- app/components/AmountCard.tsx
+++ app/components/AmountCard.tsx
@@ -22,6 +22,11 @@ const colorOn = [
     "bg-indigo-200",
 ]
 
+const deleteIcon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-4">
+  <path fillRule="evenodd" d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z" clipRule="evenodd" />
+</svg>
+
+
 // Fixed: Accept proper props instead of array type
 const AmountCard = ({m, handleDelete}: AmountCardProps) => {
     return (
@@ -30,10 +35,12 @@ const AmountCard = ({m, handleDelete}: AmountCardProps) => {
             className={`${colorOn[m.color]} rounded-2xl p-6 flex flex-col justify-between shadow-lg hover:shadow-xl transition-all duration-300`}
         >
             <div className="space-y-3">
-                <h3 className="uppercase font-bold text-red text-sm tracking-widest opacity-80">
-                    {m.sub}
-                </h3>
-                <button className="btn" onClick={() => handleDelete(m.id)}>delete</button>
+                <div className='flex items-center justify-between'>
+                    <h3 className="uppercase font-bold text-red text-sm tracking-widest opacity-80">
+                        {m.sub}
+                    </h3>
+                    <button className="cursor-pointer duration-200 hover:bg-red-300 p-1 rounded-md" onClick={() => handleDelete(m.id)}>{deleteIcon}</button>
+                </div>
 
                 <div className="flex items-end gap-1">
                     <span className="text-4xl font-bold">
